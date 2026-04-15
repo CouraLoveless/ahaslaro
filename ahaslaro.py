92959d265fde58c0a4f966a3314fe2e33c1a9b70
Vincent Cajigal <vincecajigal@gmail.com>
2026-04-15 21:18:08 +0800

    main loop

--- ahaslaro.py
+++ ahaslaro.py
@@ -5,3 +5,19 @@ wn.title("Ahas Laro")
 wn.bgcolor("white")
 wn.setup(width=800, height=800)
 wn.tracer(0)
+
+#snake
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+#main
+while True:
+    wn.update()
+
+wn.mainloop()
+
